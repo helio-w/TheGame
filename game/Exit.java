56c25a788a62311481fb37588acb7698204a488b
Maliochka <mael@cemia.fr>
2023-11-21 14:36:15 +0100

    affichage des Exit depuis une Location

--- game/Exit.java
+++ game/Exit.java
@@ -1,12 +1,16 @@
 package game;
 
 public class Exit {
-    public final String NAME;
     public final String DESCRIPTION;
+    private final Location DEST;
 
-    public Exit(String name, String desc){
-        this.NAME = name;
+    public Exit(String name, String desc, Location dest){
         this.DESCRIPTION = desc;
+        this.DEST = dest;
+    }
+
+    public String toString(){
+        return "Destination : "+this.DEST.NAME+"\nDescription :\n"+this.DESCRIPTION;
     }
     
 }
--- game/Location.java
+++ game/Location.java
@@ -18,9 +18,27 @@ public class Location{
 
     public void showExits(){
         for(Exit ex : exits){
-            ex.
+            System.out.println(ex);
         }
+    }
 
+    public void addExit(Exit e){
+        this.exits.add(e);
+    }
+
+    public List<Exit> getExits(){
+        return exits;
+    }
+
+
+    public static void main(String[] args){
+        Location l1 = new Location("L1", "Location 1");
+        Location l2 = new Location("L2", "Location 2");
+
+        Exit e = new Exit("e1", "Sortie vers L2", l2);
+
+        l1.addExit(e);
+        l1.showExits();
     }
 
     
