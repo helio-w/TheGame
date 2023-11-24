f24d16ff83b3cbe44678b3d89d7fcd9e9cee3383
Maliochka <maeld@laposte.net>
2023-11-24 17:03:24 +0100

    gestion d'erreurs

--- map/Location.java
+++ map/Location.java
@@ -28,6 +28,9 @@ public class Location{
         if(this.exits.containsKey(id)){
             System.out.println("Error addExit : key already present");
         }
+        if(e.LOC != this){
+            System.out.println("Error addExit : incorrect location of "+e.NAME);
+        }
         if(e.DEST != this){
             this.exits.put(id, e);
         }
@@ -44,10 +47,28 @@ public class Location{
     public static void main(String[] args){
         Location l1 = new Location("L1", "Sorties dispo : L2 et L3");
         Location l2 = new Location("L2", "Sorties dispo : L1 et L4");
-        Location l3 = new Location("L3", "Sporties dispo : L4");
+        Location l3 = new Location("L3", "Sorties dispo : L4");
         Location l4 = new Location("L4", "Sorties dispo : L3");
 
-        
+        Exit e1 = new Exit("E1", "Sortie vers L2", l1, l2);
+        Exit e2 = new Exit("E2", "Sortie vers L3", l1, l3);
+
+        Exit e3 = new Exit("E3", "Sortie vers L1", l2, l1);
+        Exit e4 = new Exit("E4", "Sortie vers L4", l2, l4);
+
+        Exit e5 = new Exit("E5", "Sortie vers L4", l3, l4);
+
+        Exit e6 = new Exit("E6", "Sortie vers L3", l4, l3);
+
+        l1.addExit(e1, 1);
+        l1.addExit(e2, 2);
+
+        l2.addExit(e3, 1);
+        l2.addExit(e4, 2);
+
+        l3.addExit(e5, 1);
+
+        l4.addExit(e6, 1);
     }
 
     
