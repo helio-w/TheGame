26d2d158c76b9ba3d68fc815007376a4a2ca8864
Maliochka <maeld@laposte.net>
2023-11-24 15:56:34 +0100

    ajout attribut

--- map/Exit.java
+++ map/Exit.java
@@ -4,15 +4,16 @@ public class Exit {
     public final String DESCRIPTION;
     public final String NAME;
     public final Location DEST;
+    public final Location LOC;
 
-    public Exit(String name, String desc, Location dest){
+    public Exit(String name, String desc, Location loc, Location dest){
         this.DESCRIPTION = desc;
         this.NAME = name;
         this.DEST = dest;
+        this.LOC = loc;
     }
 
     public String toString(){
         return "Nom : "+this.NAME+"\nDestination : "+this.DEST.NAME+"\nDescription :\n"+this.DESCRIPTION;
     }
-    
 }
