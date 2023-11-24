ea8427d3b27efc046fc26dc911e8da0d2ca9bb26
Maliochka <maeld@laposte.net>
2023-11-24 15:57:05 +0100

    ajout methode open()

--- map/ExitKey.java
+++ map/ExitKey.java
@@ -1,19 +1,29 @@
 package map;
+import objects.Item;
 
 public class ExitKey extends Exit{
     private boolean isLock;
+    private final Item key;
     
-    public ExitKey(String name, String desc, Location dest, boolean lock){
-        super(name, desc, dest);
+    public ExitKey(String name, String desc, Location loc, Location dest, boolean lock, Item k){
+        super(name, desc, loc,dest);
         this.isLock = lock;
+        this.key = k;
     }
     
+    @Override
     public String toString(){
         String lock = this.isLock?"YES":"NO"; //if isLoack then "YES" else "NO"
         return "Name : "+this.NAME+"\nDestination : "+this.DEST.NAME+"Lock : "+lock+"\nDescription :\n"+this.DESCRIPTION;
     }
-/* 
-    public openExit(){
-
+/*
+    public void open(){
+        Character hero = super.LOC.getCharac().get(1);
+        Item keyExit = hero.
+        if(keyExit != null){
+            isLock = false;
+        }else{
+            System.out.println("La porte est vérouillée");
+        }
     }*/
 }
