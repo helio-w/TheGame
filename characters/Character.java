c089a634a5da4aeba82d20610066f5f93fb39e5d
Maliochka <maeld@laposte.net>
2023-11-25 15:39:37 +0100

    ajout location character

--- characters/Character.java
+++ characters/Character.java
@@ -1,12 +1,15 @@
 package characters;
+import map.Location;
 
 public abstract class Character{
     public final String NAME;           // Name of the caracter
-    public int hp;                      // Health point of the caracter
+    public int hp;                // Health point of the caracter
+    public Location loc;
 
-    public Character(String name, int hp){
+    public Character(String name, int hp, Location l){
         this.NAME = name;
         this.hp = hp;
+        this.loc = l;
     }
 
 }
