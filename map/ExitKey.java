21c672bbb46313e5885c904a865e185ac81aecb6
Maliochka <maeld@laposte.net>
2023-11-25 15:40:53 +0100

    fin methode open()

--- map/ExitKey.java
+++ map/ExitKey.java
@@ -1,14 +1,16 @@
 package map;
 import objects.Item;
+import characters.Character;
+import characters.Player;
 
 public class ExitKey extends Exit{
     private boolean isLock;
-    private final Item key;
+    private final Item KEY;
     
     public ExitKey(String name, String desc, Location loc, Location dest, boolean lock, Item k){
         super(name, desc, loc, dest);
         this.isLock = lock;
-        this.key = k;
+        this.KEY = k;
     }
     
     @Override
@@ -16,14 +18,17 @@ public class ExitKey extends Exit{
         String lock = this.isLock?"YES":"NO"; //if isLoack then "YES" else "NO"
         return "Name : "+this.NAME+"\nDestination : "+this.DEST.NAME+"\nLock : "+lock+"\nDescription :\n"+this.DESCRIPTION;
     }
-/*
+
+    /**
+     * Unlock the exit if the player has the right item
+     * @param id : id of the item
+     */
     public void open(){
-        Character hero = super.LOC.getCharac().get(1);
-        Item keyExit = hero.
-        if(keyExit != null){
+        Character hero = super.LOC.getCharac().get(1); // Hero is in first position
+        if(((Player)hero).inventory.contains(KEY)){
             isLock = false;
         }else{
             System.out.println("La porte est vérouillée");
         }
-    }*/
+    }
 }
