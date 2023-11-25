f0dd9e49fe2d75531352b0e882fe83283ceb812d
Maliochka <maeld@laposte.net>
2023-11-25 14:52:09 +0100

    constructeur Character

--- characters/Character.java
+++ characters/Character.java
@@ -3,4 +3,10 @@ package characters;
 public abstract class Character{
     public final String NAME;           // Name of the caracter
     public int hp;                      // Health point of the caracter
+
+    public Character(String name, int hp){
+        this.NAME = name;
+        this.hp = hp;
+    }
+
 }
