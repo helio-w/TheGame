14de2c08cad4b1dc4c3ac119fd30a9549c40de53
Maliochka <maeld@laposte.net>
2023-11-26 12:53:29 +0100

    commentaire + toString()

--- characters/Character.java
+++ characters/Character.java
@@ -6,10 +6,20 @@ public abstract class Character{
     public int hp;                // Health point of the caracter
     public Location loc;
 
+    /**
+     * Create a instance a Character
+     * @param name : name of the character
+     * @param hp : amount of health point of the character
+     * @param l : location of the character
+     */
     public Character(String name, int hp, Location l){
         this.NAME = name;
         this.hp = hp;
         this.loc = l;
+    }
+
+    public String toString(){
+        return "\tnom : "+this.NAME;
     }
 
 }
