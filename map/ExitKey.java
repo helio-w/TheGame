a6188dd655096e15f5810ce10a1f5259d618e9e3
Maliochka <maeld@laposte.net>
2023-11-26 12:55:17 +0100

    commentaires + getIsLock() + autres

--- map/ExitKey.java
+++ map/ExitKey.java
@@ -7,17 +7,26 @@ public class ExitKey extends Exit{
     private boolean isLock;
     private final Item KEY;
     
+    /**
+     * Create a instance of ExitKey
+     * @param name : name of the exit
+     * @param desc : description of the exit
+     * @param loc : location of the exit
+     * @param dest : destination of the exit
+     * @param lock : status of the exit (locked or not)
+     * @param k : item to unlock the exit
+     */
     public ExitKey(String name, String desc, Location loc, Location dest, boolean lock, Item k){
         super(name, desc, loc, dest);
         this.isLock = lock;
         this.KEY = k;
     }
-    
+    /*
     @Override
     public String toString(){
         String lock = this.isLock?"YES":"NO"; //if isLoack then "YES" else "NO"
-        return "Name : "+this.NAME+"\nDestination : "+this.DEST.NAME+"\nLock : "+lock+"\nDescription :\n"+this.DESCRIPTION;
-    }
+        return "\tName : "+this.NAME+"\n\tDestination : "+this.DEST.NAME+"\n\tLock : "+lock+"\n\tDescription :\n\t"+this.DESCRIPTION;
+    }*/
 
     /**
      * Unlock the exit if the player has the right item
@@ -25,10 +34,16 @@ public class ExitKey extends Exit{
      */
     public void open(){
         Character hero = super.LOC.getCharac().get(1); // Hero is in first position
-        if(((Player)hero).inventory.contains(KEY)){
+        if(((Player)hero).inventory.contains(KEY) || !(this.isLock)){
             isLock = false;
+            System.out.println("La porte est ouverte");
         }else{
             System.out.println("La porte est vérouillée");
         }
+    }
+
+    /**Used to obtain the status of the exit (locked or not)*/
+    public boolean getIsLock(){
+        return this.isLock;
     }
 }
