69eab9a89bb80827143385dec5f2ee6a99c1c65d
helio-w <alexandre.guillot@posteo.net>
2023-11-30 19:49:38 +0100

    Updated Use command (need test)

--- src/command/CommandUse.java
+++ src/command/CommandUse.java
@@ -1,11 +1,18 @@
 package command;
 
+import game.GameHandler;
+
 public class CommandUse extends Command {
 	public CommandUse() {
 		super("USE", "Use the object in your inv in [argument]");
 	}
 	
 	public boolean execute(String[] args) {
+		GameHandler theGame = GameHandler.getInstance();
+		if (args.length == 2) {
+			
+		}
+		
 		return false;
 	}
 }
