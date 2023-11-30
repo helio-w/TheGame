0946c71e998cb5ee6b9a9fa4fc51ff92c57e09dc
Maliochka <maeld@laposte.net>
2023-11-30 18:45:26 +0100

    createFoot

--- src/objects/Food.java
+++ src/objects/Food.java
@@ -6,6 +6,16 @@ public class Food extends Item implements Usable{
         super(name, desc, id);
     }
 
+    public static Food createFood(String n, String d, Integer id) throws Exception{
+        Food i = new Food(n, d, id);
+        if(allItems.containsKey(id)){
+            throw new Exception("Error createFood : item id already in allItems");
+        }else{
+            allItems.put(id, i);
+            return i;
+        }
+    }
+
     public void use(){
         System.out.println("Yummy !");
     }
