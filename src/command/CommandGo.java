e3b5e1b8f2380144877b04070953b6a1b2e3292f
helio-w <alexandre.guillot@posteo.net>
2023-11-30 19:34:30 +0100

    Fixed cross text issue

--- src/command/CommandGo.java
+++ src/command/CommandGo.java
@@ -13,11 +13,11 @@ public class CommandGo extends Command{
 	
 	public boolean execute(String[] args) {
 		Location curLoc = theGame.getPlayer().loc;
-		Map<String, Exit> exits = curLoc.getExits(); 					// Get the HashMap of Exits
+		Map<String, Exit> exits = curLoc.getExits(); 							// Get the HashMap of Exits
 		if(args.length == 2) {
-			if(exits.containsKey(args[1].toUpperCase())) {				// Checking if Str in args[1] is a valid exit name
-				theGame.getPlayer().move(args[1]);
-				// TODO Implement lockedExit	
+			if(exits.containsKey(args[1].toUpperCase())) {						// Checking if Str in args[1] is a valid exit name (every exit is upper case)
+				theGame.getPlayer().move(args[1].toUpperCase());
+				System.out.println(exits.get(args[1].toUpperCase()).TXTCROSS); 	// Printing txt on console when exit is crossed
 				return true;
 			}else {
 				Utils.printErr("Error GO : The Destination does not exist !");
