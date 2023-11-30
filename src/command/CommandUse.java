fb89f3a97a7c92843db039ea4bb09ed3593c460a
helio-w <alexandre.guillot@posteo.net>
2023-11-30 18:47:33 +0100

    Added command Use (not working)

--- src/command/CommandUse.java
+++ src/command/CommandUse.java
@@ -1,5 +1,11 @@
 package command;
 
 public class CommandUse extends Command {
-
+	public CommandUse() {
+		super("USE", "Use the object in your inv in [argument]");
+	}
+	
+	public boolean execute(String[] args) {
+		return false;
+	}
 }
